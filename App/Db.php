b1dc8cd6b787087ae3ec64f7b045d3562428d029
Ilya B <ilbv@mail.ru>
2020-03-30 01:22:25 +0300

    added Exception for Db query

--- App/Db.php
+++ App/Db.php
@@ -3,6 +3,8 @@
 namespace App;
 
 
+use App\Exceptions\DbException;
+
 class Db
 {
     protected $dbh;
@@ -17,7 +19,10 @@ class Db
     public function query($sql, $data = [], $class)
     {
         $sth = $this->dbh->prepare($sql);
-        $sth->execute($data);
+        $res = $sth->execute($data);
+        if (!$res) {
+            throw new DbException($sql, 'Запрос не может быть выполнен');
+        }
         return $sth->fetchAll(\PDO::FETCH_CLASS, $class);
     }
 
