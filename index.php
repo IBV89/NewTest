a2b2f9c77b4889f482ac65f313829c89d5095aea
Ilya B <ilbv@mail.ru>
2020-04-23 01:45:18 +0300

    added \App\Controllers

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
 <?php
-
+require_once 'vendor/autoload.php';
 require_once __DIR__ . '/autoload.php';
 
-$ctrl = $_GET['ctrl'] ?? 'Index';
-$class = '\App\Controllers\\' . $ctrl;
+try {
 
-$ctrl = new $class;
-$ctrl();
+    $ctrl = $_GET['ctrl'] ?? 'Index';
+    $class = '\App\Controllers\\' . $ctrl;
+
+    $ctrl = new $class;
+    $ctrl();
+} catch (\App\Exceptions\DbException $error) {
+    echo 'Ошибка при выполнении запроса (' . $error->getQuery() . ' ) : ' . $error->getMessage();
+    die();
+} catch (\App\Exceptions\ArticleException $error) {
+    echo $error->getError();
+} catch (Exception $error) {
+    echo 'Неизвестная ошибка';
+    die();
+}
